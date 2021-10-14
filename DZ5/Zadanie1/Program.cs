250b80fb7d50ecc7c6d3429bc71407998b2577d0
Gislam05 <g_islam05@mail.ru>
2021-10-14 19:52:48 +0300

    Домашнее задание 5

---
+++ DZ5/Zadanie1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace DZ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("startup.txt", "New Text");
+            
+        }
+    }
+}
---
+++ DZ5/Zadanie3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace DZ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] array = new byte[10];
+            for (int i = 0; i < 10; i++)
+                array[i] = byte.Parse(Console.ReadLine());
+            File.WriteAllBytes("bytes.bin", array);
+           
+        }
+    }
+}
