cf2ef06d7662cb0b4b95cd54d228a25260da440c
Gislam05 <g_islam05@mail.ru>
2021-10-18 20:56:28 +0300

    Домашнее задание 6

---
+++ DZ6/DZ6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DZ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                Console.WriteLine(processes[i].Id + " "+ processes[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Какой процесс хочешь убить? Введи его ID");
+            int id = int.Parse (Console.ReadLine());
+            Process.GetProcessById(id).Kill();
+            Console.WriteLine($"{Process.GetProcessById(id).ProcessName} завершен");
+        }
+    }
+}
