6527f3a250f18e768f667fa258af89373f2baca9
Gislam05 <g_islam05@mail.ru>
2021-11-01 19:51:37 +0300

    DZ1

---
+++ 2X/DZ1/DZ2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DZ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int StrangeSum(int[] inputArray)
+            {
+                int sum = 0; //О(1)
+                for (int i = 0; i < inputArray.Length; i++) 
+                {
+                    for (int j = 0; j < inputArray.Length; j++) //O(n)
+                    {
+                        for (int k = 0; k < inputArray.Length; k++) //O(n)*O(n)
+                        {
+                            int y = 0; 
+
+                            if (j != 0)
+                            {
+                                y = k / j; //O(n)*(O(n)-1)*O(n)
+                            }
+
+                            sum += inputArray[i] + i + k + j + y; //O(n)*O(n)*O(n)
+                        }
+                    }
+                }
+
+                return sum; //Если N - длина входного массива, то сложность получается O^3(N)
+            }
+        }
+    }
+}
---
+++ 2X/DZ1/DZ3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(F(int.Parse(Console.ReadLine())));
+            ulong F(int n)
+            {
+                if (n == 1 || n == 2)
+                {
+                    return 1;
+                }
+                return F(n-1)+F(n-2);
+            }
+            Console.WriteLine("Теперь без использования функции. Введите номер: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1, j=1, k=0;
+            for (; k < n/2; )
+            {
+                i += j; j += i;
+                k += 1;
+                Console.WriteLine(i + ", "+j + ", ");
+            }
+            Console.WriteLine("Номер Фибоначчи: "+j);
+
+        }
+    }
+}
