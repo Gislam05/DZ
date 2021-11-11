001f9cb4118c0cd952f782ee8cc7fedf172d39b1
Gislam05 <g_islam05@mail.ru>
2021-11-11 18:56:15 +0300

    Обновление

--- 2X/DZ3/DZ3/Program.cs
+++ 2X/DZ3/DZ3/Program.cs
@@ -1,55 +1,126 @@
 ﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace DZ3
 {
     class Program
     {
-		public class PointClass
-		{
-			public int X;
-			public int Y;
-		}
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
 
-		public struct PointStruct
-		{
-			public int X;
-			public int Y;
-		}
+    public class PointClass
+    {
+        public int X;
+        public int Y;
+    }
 
+    public struct PointStruct
+    {
+        public int X;
+        public int Y;
+    }
 
-		static void Main(string[] args)
-		{
-			var point = new PointStruct { X = 42, Y = 42 };
-			var array = new PointStruct[1];
+    public class BechmarkClass
+    {
+        [Params(0, 1, 2)] //Новое
+        public int AIdx { get; set; }
 
-			array[0] = point;
-			array[0].Y = 33;
+        [Params(0, 1, 2)] //Новое
+        public int BIdx { get; set; }
 
-			PrintPoint(point, "1. point local variable");
-			PrintPoint(array[0], "2. point in array");
+        private PointClass[] clArray = ClassArray(); //Объявлен массив типа PointClass
+        private PointStruct[] stArray = StructArray();//Объявлен массив типа PointStruct
 
-			ChangePoint(point);
+        public static PointClass[] ClassArray()
+        {
+            var rand = new Random(100);
+            PointClass[] arrayPointClass = new PointClass[3];
+            for (int i = 0; i < 3; i++)
+            {
+                PointClass arrGenClass = new PointClass() { X = 1 * i, Y = 5 * i };
+                arrayPointClass[i] = arrGenClass;
+            }
 
-			PrintPoint(point, "3. point local variable");
-			PrintPoint(array[0], "4. point in array");
-
-			point = new PointStruct() { X = 7, Y = 7 };
-
-			PrintPoint(point, "5. point local variable");
-			PrintPoint(array[0], "6. point in array");
-		}
-
-		public static void ChangePoint(PointStruct pointClass)
-		{
-			pointClass.X = 13;
-			pointClass.Y = 13;
-		}
-
-		public static void PrintPoint(PointStruct pointClass, string tag)
-		{
-			Console.WriteLine($"{tag}\t X:{pointClass.X}, Y:{pointClass.Y}");
-		}
+            return arrayPointClass;
+        }
 
 
-	}
+        public static PointStruct[] StructArray()
+        {
+            var rand = new Random(100);
+            PointStruct[] arrayPointStruct = new PointStruct[3];
+            for (int i = 0; i < 3; i++)
+            {
+                PointStruct arrGenClass = new PointStruct() { X = 1 * i, Y = 5 * i };
+                arrayPointStruct[i] = arrGenClass;
+            }
+            return arrayPointStruct;
+        }
+
+
+        public static float PointDistanceClass(PointClass pointOne, PointClass pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+        public static float PointDistance(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        public static float PointDistanceShort(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return (x * x) + (y * y);
+        }
+
+        public static double PointDistanceDouble(PointStruct pointOne, PointStruct pointTwo)
+        {
+            double x = pointOne.X - pointTwo.X;
+            double y = pointOne.Y - pointTwo.Y;
+            return Math.Sqrt((x * x) + (y * y));
+        }
+
+
+        [Benchmark]
+        public void floatPointDistanceClass()
+        {
+            var pointOne = clArray[AIdx];
+            var pointTwo = clArray[BIdx];
+            PointDistanceClass(pointOne, pointTwo);
+
+        }
+
+        [Benchmark]
+        public void floatPointDistance()
+        {
+            var pointOne = stArray[AIdx];
+            var pointTwo = stArray[BIdx];
+            PointDistance(pointOne, pointTwo);
+        }
+
+        [Benchmark]
+        public void PointDistanceShort()
+        {
+            var pointOne = stArray[AIdx];
+            var pointTwo = stArray[BIdx];
+            PointDistanceShort(pointOne, pointTwo);
+        }
+
+        [Benchmark]
+        public void PointDistanceDouble()
+        {
+            var pointOne = stArray[AIdx];
+            var pointTwo = stArray[BIdx];
+            PointDistanceDouble(pointOne, pointTwo);
+        }
+    }
 }
